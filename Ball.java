9bb1386616ac340eca1f1efc48fdb1db748b347f
Bryant Park <52787152+bryantpark04@users.noreply.github.com>
2019-07-26 14:43:26 -0400

    add getters and setters to Ball

--- Ball.java
+++ Ball.java
@@ -1,3 +1,51 @@
 public class Ball
 {
+	private int myX, myY, mydx = 0, mydy = 0;	
+	public Ball(int x, int y)
+	{
+		myX = x;
+		myY = y;
+	}
+	
+	// instance methods
+	public void draw(Graphics g)
+	{
+		
+	}
+	
+	// getters
+	public int getX()
+	{
+		return myX;
+	}
+	public int getY()
+	{
+		return myY;
+	}
+	public int getdx()
+	{
+		return mydx;
+	}
+	public int getdy()
+	{
+		return mydy;
+	}
+	
+	// setters
+	public void setX(int x)
+	{
+		myX = x;
+	}
+	public void setY(int y)
+	{
+		myY = y;
+	}
+	public void setdx(int dx)
+	{
+		mydx = dx;
+	}
+	public void setdy(int dy)
+	{
+		mydy = dy;
+	}
 }
