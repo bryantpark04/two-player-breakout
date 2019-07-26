c4b8c08ccc256ae410eba5295ac69f86db6c7a75
Bryant Park <52787152+bryantpark04@users.noreply.github.com>
2019-07-26 14:13:56 -0400

    Add files via upload
    added methods to read and write high scores

--- Scoreboard.java
+++ Scoreboard.java
@@ -9,14 +9,33 @@ public class Scoreboard extends JPanel
 		
 	}
 	
-	private int loadHighScore()
+	private int loadHighScore() 	// loads high score from a txt file
 	{
-		Scanner s = new Scanner(new File("data.txt"));
-		int hs = s.nextInt();
-		s.close();
-	}
-	private void storeHighScore(int hs)
-	{
+		int hs = 0;
 		
+		try
+		{
+			Scanner s = new Scanner(new File("data.txt"));
+			hs = s.nextInt();
+			s.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("Data file not found! High score set to 0.");
+		}
+		
+		return hs;
+	}
+	private void storeHighScore(int hs) 	// writes high score to a txt file - should be called whenever the high score updates
+	{
+		try
+		{
+			PrintWriter p = new PrintWriter(new FileWriter("data.txt"));
+			p.println(hs);
+		}
+		catch(IOException e)
+		{
+			System.out.println("High score could not be written to file! High score will be set to 0 on next run.");
+		}
 	}
 }
