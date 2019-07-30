cb3e12ed9f843a93a0d7cd9de7db39c07e294673
Bryant Park <52787152+bryantpark04@users.noreply.github.com>
2019-07-30 09:54:00 -0400

    Revert "Merge pull request #17 from bryantpark04/greg-endgame"
    This reverts commit 26d9797695fa3c009a1c44ffceff4077dfb75255, reversing
    changes made to b6a40f3a90f7386ad4d78c3ee4bd1d812228c93c.

--- Display.java
+++ Display.java
@@ -11,6 +11,7 @@ public class Display extends JPanel
    private Slider slider;
    private Block[][] blocks = new Block[10][6];
    private Key k = new Key();
+   private int level = 0;
    public boolean on1, on2, on3, on4;
    public Display() { 	// constructor
       myImage = new BufferedImage(Driver.WIDTH, Driver.HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -31,7 +32,7 @@ public class Display extends JPanel
          }
       }
       
-
+   
       
       addKeyListener(k); 	// add key listener
       setFocusable(true);
@@ -40,72 +41,90 @@ public class Display extends JPanel
       t.start();
    }
 	
-	public void stopGame() {
-		ball.setdx(0);
-		ball.setdy(0);
-		removeKeyListener(k);
-		on1 = false;
-		on2 = false;
-		on3 = false;
-		on4 = false;
-	}
+   public void stopGame() {
+      ball.setdx(0);
+      ball.setdy(0);
+      removeKeyListener(k);
+      on1 = false;
+      on2 = false;
+      on3 = false;
+      on4 = false;
+   }
+   public void reset() {
+      for(int r = 0; r < blocks.length; r++) {
+         for(int c = 0; c < blocks[0].length; c++) {
+            blocks[r][c].setX(1 + 60 * r);
+            blocks[r][c].setY(1 + 30 * c);
+         }
+      }
+      
+      ball.setX(300);
+      ball.setY(300);
+      ball.setdx(0);
+      ball.setdy(3 + level);
+   	
+      slider.setX(270);
+      slider.setY(550);
+      
+      level++;
+   }
 	
 	// boolean methods
-	public boolean gameEnd() {
-		if(ball.hitsBottom()) {
-			return true;
-		}
-		return false;
-	}
-	public boolean pointScored() {
-		for(int r = 0; r < blocks.length; r++) {
-			for(int c = 0; c < blocks[0].length; c++) {
-				if(ball.getX() + ball.getDiameter() > blocks[r][c].getX() && ball.getX() < blocks[r][c].getX() + blocks[r][c].getWidth()) {
-					if(ball.getY() + ball.getDiameter() > blocks[r][c].getY() && ball.getY() < blocks[r][c].getY() + blocks[r][c].getHeight()) {
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}
-	public boolean roundWon() {
-		for(int r = 0; r < blocks.length; r++) {
-			for(int c = 0; c < blocks[0].length; c++) {
-				if(blocks[r][c].getX() != -100) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+   public boolean gameEnd() {
+      if(ball.hitsBottom()) {
+         // return true;
+      }
+      return false;
+   }
+   public boolean pointScored() {
+      for(int r = 0; r < blocks.length; r++) {
+         for(int c = 0; c < blocks[0].length; c++) {
+            if(ball.getX() + ball.getDiameter() > blocks[r][c].getX() && ball.getX() < blocks[r][c].getX() + blocks[r][c].getWidth()) {
+               if(ball.getY() + ball.getDiameter() > blocks[r][c].getY() && ball.getY() < blocks[r][c].getY() + blocks[r][c].getHeight()) {
+                  return true;
+               }
+            }
+         }
+      }
+      return false;
+   }
+   public boolean roundWon() {
+      for(int r = 0; r < blocks.length; r++) {
+         for(int c = 0; c < blocks[0].length; c++) {
+            if(blocks[r][c].getX() != -100) {
+               return false;
+            }
+         }
+      }
+      return true;
+   }
 	
 	// collision helpers
-	private void collisionSlider(Ball b) {
-		if(collision(slider, b)) {
-			b.setdy(-1 * b.getdy());
-		}
-	}
-	private void collisionBlocks(Ball b) {
-		for(int r = 0; r < blocks.length; r++) {
-			for(int c = 0; c < blocks[0].length; c++) {
-				if(collision(blocks[r][c], b)) {
-					blocks[r][c].setX(-100);
-					blocks[r][c].setY(-100);
-					b.setdy(-1 * b.getdy());
-					b.setdx(Math.random() * 4 - 2);
-				}
-			}
-		}
-	}
-	private boolean collision(Rectangle r, Ball b) {
-		if(b.getX() + b.getDiameter() > r.getX() && b.getX() < r.getX() + r.getWidth()) {
-			if(b.getY() + b.getDiameter() > r.getY() && b.getY() < r.getY() + r.getHeight()) {
-				return true;
-			}
-		}
-		return false;
-	}
+   private void collisionSlider(Ball b) {
+      if(collision(slider, b)) {
+         b.setdy(-1 * b.getdy());
+      }
+   }
+   private void collisionBlocks(Ball b) {
+      for(int r = 0; r < blocks.length; r++) {
+         for(int c = 0; c < blocks[0].length; c++) {
+            if(collision(blocks[r][c], b)) {
+               blocks[r][c].setX(-100);
+               blocks[r][c].setY(-100);
+               b.setdy(-1 * b.getdy());
+               b.setdx(Math.random() * (4 + level) - 2);
+            }
+         }
+      }
+   }
+   private boolean collision(Rectangle r, Ball b) {
+      if(b.getX() + b.getDiameter() > r.getX() && b.getX() < r.getX() + r.getWidth()) {
+         if(b.getY() + b.getDiameter() > r.getY() && b.getY() < r.getY() + r.getHeight()) {
+            return true;
+         }
+      }
+      return false;
+   }
 	
    // Listeners
    private class Listener implements ActionListener {
@@ -135,8 +154,8 @@ public class Display extends JPanel
                   blocks[r][c].draw(buffer);
                }
             }
-
-
+         
+         
          }
          catch(NullPointerException n) {
          	
--- Panel.java
+++ Panel.java
@@ -3,47 +3,35 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Panel extends JPanel {
-private Display display;
-public JFrame f;
-public JFrame fr;
-
-private Scoreboard scoreboard;
-	public Panel(JFrame l) {
-		f=l;
+   private Display display;
+   private Scoreboard scoreboard;
+   public Panel() {
+   	
       display = new Display();
       scoreboard = new Scoreboard();
-		setLayout(new BorderLayout());
-		
-		add(display, BorderLayout.CENTER);
-		add(scoreboard, BorderLayout.SOUTH);
-		
-		Timer t = new Timer(10, new Listener());
-		t.start();
-      fr = new JFrame("Breakout2");
-		fr.setSize(625, 320);
-		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		fr.setLocationRelativeTo(null);
-		fr.setContentPane(new EndPanel());
-      fr.setVisible(false);
-
-      
-	}
-   
+      setLayout(new BorderLayout());
+   	
+      add(display, BorderLayout.CENTER);
+      add(scoreboard, BorderLayout.SOUTH);
+   	
+      Timer t = new Timer(10, new Listener());
+      t.start();
+   }
 	
-	private class Listener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			if(display.gameEnd()) {
-				scoreboard.storeHighScore();
-				display.stopGame();
-            f.setVisible(false);
-            fr.setVisible(true);
-  			}
-			if(display.pointScored()) {
-				scoreboard.incrementScore();
-			}
-			if(display.roundWon()) {
-				JOptionPane.showMessageDialog(null, "You won!");
-			}
-		}
-	}
+   private class Listener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         if(display.gameEnd()) {
+            scoreboard.storeHighScore();
+            display.stopGame();
+            JOptionPane.showMessageDialog(null, "You lost! Your score was: " + scoreboard.getScore());
+            System.exit(0);
+         }
+         if(display.pointScored()) {
+            scoreboard.incrementScore();
+         }
+         if(display.roundWon()) {
+            display.reset();
+         }
+      }
+   }
 }
