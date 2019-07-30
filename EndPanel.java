ab93956fafac031d1daf6ef3f1c8bfbcbc7fdb6d
TheBest0422 <53340798+TheBest0422@users.noreply.github.com>
2019-07-30 09:48:34 -0400

    game over screen at end

---
+++ EndPanel.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+
+public class EndPanel extends JPanel
+{
+   public JFrame f;
+   private BufferedImage myImage;
+   private Graphics myBuffer;
+
+   public EndPanel()
+   {
+      
+      setLayout(new BorderLayout());
+      myImage = new BufferedImage(640, 360, 1);
+      myBuffer = myImage.getGraphics();
+      JButton end=new JButton("Quit");
+      end.addActionListener(new Listener());
+      add(end,BorderLayout.SOUTH);
+
+      
+      
+      
+      
+   }
+   private class Listener implements ActionListener
+   {
+      public void actionPerformed(ActionEvent e) {
+         System.exit(0);
+      }
+   }
+   public void paintComponent(Graphics g)
+   {
+      ImageIcon end=new ImageIcon("lel.jpg");
+      g.drawImage(end.getImage(),0,0,640,300,null);
+   }
+ }  
+   
--- Slider.java
+++ Slider.java
@@ -3,22 +3,33 @@ import java.awt.image.*;
 
 public class Slider extends Rectangle
 {
-   public Slider(int x, int y) {
-      super(x, y, 60, 10);
-   }
+	//private int myX, myY, myWidth = 60, myHeight = 10;
+	public Slider(int x, int y) {
+		super(x, y, 60, 10);
+	}
 	
 	// movement methods
 	// possible addition - dx changes depending on difficulty?
-   public void moveLeft() {	
-      setX(getX() - 5);
-      if(getX() + getWidth() / 2.0 < 0) {
-         setX((int) (Driver.WIDTH - getWidth() / 2.0));
-      }
-   }
-   public void moveRight() {
-			setX(getX() + 5);
-      if(getX() + getWidth() / 2.0 > Driver.WIDTH) {
-         setX((int) (0 - getWidth() / 2.0));
-      }
-   }
+	public void moveLeft() {
+		//myX -= 5;
+		//if(myX + myWidth / 2.0 < 0) {
+			//myX = (int) (Driver.WIDTH - myWidth / 2.0);
+		//}
+		
+		setX(getX() - 5);
+		if(getX() + getWidth() / 2.0 < 0) {
+			setX((int) (Driver.WIDTH - getWidth() / 2.0));
+		}
+	}
+	public void moveRight() {
+		//myX += 5;
+		//if(myX + myWidth / 2.0 > Driver.WIDTH) {
+			//myX = (int) (0 - myWidth / 2.0);
+		//}
+		
+		setX(getX() + 5);
+		if(getX() + getWidth() / 2.0 > Driver.WIDTH) {
+			setX((int) (0 - getWidth() / 2.0));
+		}
+	}
 }
